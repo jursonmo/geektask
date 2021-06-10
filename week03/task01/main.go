6686a4efd6dab48df546cccf573002bbb03cbe4a
william <myname@example.com>
2021-06-10 12:17:05 +0800

    week03 task

---
+++ week03/task01/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	eg, ctx := errgroup.WithContext(context.Background())
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("hello world"))
+	})
+	server := http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+
+	eg.Go(func() error {
+		err := server.ListenAndServe()
+		return fmt.Errorf("server.ListenAndServe err:%w", err) //%w, wrap err with msg
+	})
+
+	//shutdown http server
+	eg.Go(func() error {
+		select {
+		case <-ctx.Done():
+			timeoutCtx, _ := context.WithTimeout(context.Background(), time.Second*3)
+			err := server.Shutdown(timeoutCtx)
+			fmt.Println("server.Shutdown err:", err)
+			return ctx.Err()
+		}
+	})
+
+	//listen signal
+	eg.Go(func() error {
+		quit := make(chan os.Signal)
+		signal.Notify(quit, os.Interrupt, os.Kill)
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case s := <-quit:
+			fmt.Printf("get signal:%v and quit\n", s)
+			return fmt.Errorf("get signal:%v and quit", s)
+		}
+	})
+
+	//wait all task quit
+	fmt.Println(eg.Wait().Error())
+}
