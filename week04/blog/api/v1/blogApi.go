c54b37b50915097266b9cdb8a4eb48be18e3bd59
william <myname@example.com>
2021-06-24 11:30:22 +0800

    comment

--- week04/blog/api/v1/blogApi.go
+++ week04/blog/api/v1/blogApi.go
@@ -1,9 +1,9 @@
 package v1
 
-//response data Struct
+//response data Struct, TODO: error 转换处理
 type ArticleDTO struct {
-	Err     error `json:"err"`
-	Id      int64 `json:"ArticleID"`
-	Title   string
-	Content []byte
+	Err     error  `json:"err"`
+	Id      int64  `json:"id"`
+	Title   string `json:"title"`
+	Content []byte `json:"content"`
 }
